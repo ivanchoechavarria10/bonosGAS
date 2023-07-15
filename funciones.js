8904e0909c2132102617f51b81635b59cbe59bb4
ivanchoechavarria10 <ivanchoechavarria@gmail.com>
2023-07-15 07:19:33 -0500

    video 10 curso gas-avanzado

--- funciones.js
+++ funciones.js
@@ -5,3 +5,9 @@ function doGet(){
 function obtenerDatosHTML(nombre){
     return HtmlService.createHtmlOutputFromFile(nombre).getContent();
 }
+
+function obtenerContactos() {
+    let hoja = SpreadsheetApp.openById('1tbqG7Qhh6kYtQLCCFrn4rr--tKm9psc213ZFoImhsj4').getActiveSheet();
+    let datos = hoja.getDataRange().getValues();
+    return datos;
+}
